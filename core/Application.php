5938a2d9724338648feee946a1eb684ba5ed09ce
Abdullaev Alisultan <sultan7306@mail.ru>
2022-03-04 15:19:46 +0300

    Modified application class

--- core/Application.php
+++ core/Application.php
@@ -3,5 +3,15 @@ namespace app\core;
 
 class Application
 {
+    public Router $router;
 
+    public function __construct($ROOT_DIR)
+    {
+        $this->router = new Router();
+    }
+
+    public function run()
+    {
+        echo $this->router->resolve();
+    }
 }
